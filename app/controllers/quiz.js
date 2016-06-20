dbd34b003422d9919de524c7b90ba289dcac0ad1
roxalech <roxalech@gmail.com>
2016-06-20 17:11:56 +0300

    worked on take-quiz template, need design touches

--- app/controllers/quiz.js
+++ app/controllers/quiz.js
@@ -20,12 +20,12 @@ function getQuizData (req, res, next) {
   if(isNaN(data.questions)) {
     return res.status(401).json({ message: "The number of questions should be a number" });
   }
-console.log(data.questions);
-  console.log(typeof data.questions);
-  console.log('isnan', isNaN(data.questions))
+//console.log(data.questions);
+//  console.log(typeof data.questions);
+//  console.log('isnan', isNaN(data.questions))
 
   req.resources.nrOfQuestions = parseInt(data.questions);
-console.log(typeof req.resources.nrOfQuestions)
+//console.log(typeof req.resources.nrOfQuestions)
   req.resources.questionTypes = {
     categories: data.category.split(","),
     difficulty: data.difficulty
@@ -42,10 +42,7 @@ function getQuizQuestions (req, res, next) {
   query.category = {
     $in: categories
   }
-
   query.difficulty = difficultyLvl;
-
-  console.log(query);
 
   Question
   .find(query)
@@ -55,7 +52,7 @@ function getQuizQuestions (req, res, next) {
       return next(err);
     }
 
-    console.log('res', results);
+    //console.log('res', results);
     req.resources.quizQuestions = results;
     next();
   });
@@ -97,41 +94,47 @@ function generateQuiz (req, res, next) {
   var questions = req.resources.quizQuestions;
   var finalQuestions = [];
   var chosenNr = [];
+  req.session.historyData = {};
 
   if (nrOfQuestions > questions.length) {
     req.session.historyData.errorMessage = "There aren't enough questions matching your parameters";
-    res.json(req.session.historyData.errorMessage);
+    //res.json(req.session.historyData.errorMessage);
+    nrOfQuestions = questions.length;
   }
 
-  if (nrOfQuestions > 50) {
+  if (nrOfQuestions > 20) {
     req.session.historyData.errorMessage = "The number of questions  exceeds the maximum limit";
-    nrOfQuestions = 50;
-    res.json(req.session.historyData.errorMessage);
+    nrOfQuestions = 20;
+    //res.json(req.session.historyData.errorMessage);
 
   } else if (nrOfQuestions < 1) {
     req.session.historyData.errorMessage = "The number of questions  is unde the minimum limit";
     nrOfQuestions = 1;
-    res.json(req.session.historyData.errorMessage);
+    //res.json(req.session.historyData.errorMessage);
   }
-console.log(nrOfQuestions);
-  console.log(finalQuestions.length);
+  //console.log('nrOfQuestions', nrOfQuestions);
+  //console.log('finallen', finalQuestions.length);
+  //console.log('resulted', questions.length);
+
   while (finalQuestions.length < nrOfQuestions) {
     var random = _.random(0, questions.length - 1);
 
+    //console.log(random)
+    //console.log('if', !_.includes(chosenNr, random));
+
     if (!_.includes(chosenNr, random)) {
       chosenNr.push(random);
+      //console.log('chosen', chosenNr)
       finalQuestions.push(questions[random]);
     }
   }
-  console.log('!!!!!!!', finalQuestions)
+  //console.log('!!!!!!!', finalQuestions)
   req.session.finalQuestions = finalQuestions;
   return res.redirect(304, '/quiz');
-  //next();
-  //res.json(finalQuestions);
 }
 
 function takeQuizPage (req, res) {
-  console.log('finalQ', req.session.finalQuestions);
+  //console.log('finalQ', req.session.finalQuestions);
   res.render('quiz/take-quiz', {
     questions: req.session.finalQuestions,
     historyData: req.session.historyData
